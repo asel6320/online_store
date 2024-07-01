3ead69da068946dda9e946282d889a23494d1650
Asel Amatova <asel6320@gmail.com>
2024-07-01 02:57:56 -0700

    changed database to postgres and created online_store database
    added html files, added urls.py to webapp

---
+++ webapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from webapp.views import index, create_product, product_detail
+
+urlpatterns = [
+    path('', index, name='products'),
+    path('create/', create_product, name='create_product'),
+    path('article/<int:pk>/', product_detail, name='product_detail')
+]
